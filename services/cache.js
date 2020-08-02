009135fe1330582631e0cc2adfe65d84a6896de3
Kennedy Njeri <mistakenz123@gmail.com>
2020-08-02 17:55:07 +0300

    restoring redis

--- services/cache.js
+++ services/cache.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose')
 
+
+const redis = require("redis");
+const client = redis.createClient();
+
+// promisify enables us to use promises in cachedBlogs so that we dont use callbacks, we use promises
+const util = require('util')
+client.get = util.promisify(client.get)
+
+
+
+
 // get a reference to the existing default exec function that is defined in in a query so that we override it whenever the query is executed
 // stores the original to an exec func i.e the copy
 const exec = mongoose.Query.prototype.exec
@@ -10,6 +21,9 @@ mongoose.Query.prototype.exec = function () {
 
     // console.log(this.getQuery())
     // console.log(this.mongooseCollection.name)
+    // so that we can generate one consistent key and list their collections... user keys are identical but consistent with the blog one
+    // therefore we combine them in one object
+    // object.assign is used to copy properties from one object to another
     const key = Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
     })
